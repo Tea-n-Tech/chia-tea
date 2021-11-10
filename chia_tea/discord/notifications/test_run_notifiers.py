3ada3b4ed178a87eb4a1084b35a0a47f2003b210
codie <constantin.diez@gmail.com>
2021-11-10 23:54:56 +0100

    fix lint

--- chia_tea/discord/notifications/test_run_notifiers.py
+++ chia_tea/discord/notifications/test_run_notifiers.py
@@ -6,7 +6,7 @@ from ...utils.testing import async_test
 from .run_notifiers import DISCORD_MSG_LIMIT, MSG_TOO_LONG, log_and_send_msg_if_any
 
 
-def get_future(obj):
+def _get_future(obj):
     f = asyncio.Future()
     f.set_result(obj)
     return f
@@ -18,7 +18,7 @@ class TestRunNotifiers(unittest.TestCase):
     @async_test
     async def test_msg_is_sent(self):
         channel_mock = mock.MagicMock()
-        channel_mock.send.return_value = get_future(None)
+        channel_mock.send.return_value = _get_future(None)
         logger_mock = mock.MagicMock()
         logger_mock.info.return_value = None
 
@@ -38,7 +38,7 @@ class TestRunNotifiers(unittest.TestCase):
     @async_test
     async def test_log_and_send_msg_if_any_msg_too_long(self):
         channel_mock = mock.MagicMock()
-        channel_mock.send.return_value = get_future(None)
+        channel_mock.send.return_value = _get_future(None)
         logger_mock = mock.MagicMock()
         logger_mock.info.return_value = None
 
@@ -58,7 +58,7 @@ class TestRunNotifiers(unittest.TestCase):
     @async_test
     async def test_msg_is_not_sent_to_discord_during_testing(self):
         channel_mock = mock.MagicMock()
-        channel_mock.send.return_value = get_future(None)
+        channel_mock.send.return_value = _get_future(None)
         logger_mock = mock.MagicMock()
         logger_mock.info.return_value = None
 
@@ -75,7 +75,7 @@ class TestRunNotifiers(unittest.TestCase):
     @async_test
     async def test_multiple_messages_are_bundled(self):
         channel_mock = mock.MagicMock()
-        channel_mock.send.return_value = get_future(None)
+        channel_mock.send.return_value = _get_future(None)
         logger_mock = mock.MagicMock()
         logger_mock.info.return_value = None
 
@@ -93,7 +93,7 @@ class TestRunNotifiers(unittest.TestCase):
     @async_test
     async def test_multiple_messages_are_sent_individually_if_too_long(self):
         channel_mock = mock.MagicMock()
-        channel_mock.send.return_value = get_future(None)
+        channel_mock.send.return_value = _get_future(None)
         logger_mock = mock.MagicMock()
         logger_mock.info.return_value = None
 
