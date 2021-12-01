207446fcda5b5fa4b66b5067d93e76d16dde93a5
codie <constantin.diez@gmail.com>
2021-12-01 23:12:21 +0100

    fix tests

--- chia_tea/cli/test_config.py
+++ chia_tea/cli/test_config.py
@@ -1,6 +1,5 @@
-import os
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 from typer.testing import CliRunner
 
@@ -10,23 +9,9 @@ from .main import app
 runner = CliRunner()
 
 
-def assert_certificate_generated(
-    dirpath: str, overwrite: bool, create_certificate_pair_mock: MagicMock
-):
-    """Asserts that the certificate generation fn was called correctly"""
-    key_path = os.path.join(dirpath, "server.key")
-    cert_path = os.path.join(dirpath, "server.crt")
-    create_certificate_pair_mock.assert_called_once_with(
-        key_path=key_path, cert_path=cert_path, overwrite=overwrite
-    )
-
-
 class TestConfigCmd(unittest.TestCase):
-    @patch("chia_tea.cli.config.create_certificate_pair")
     @patch("chia_tea.cli.config.create_default_config")
-    def test_config_init_creates_default_config_correctly(
-        self, create_default_config_mock, create_certificate_pair_mock
-    ):
+    def test_config_init_creates_default_config_correctly(self, create_default_config_mock):
         overwrite = False
 
         result = runner.invoke(app, ["config", "init"])
@@ -34,19 +19,11 @@ class TestConfigCmd(unittest.TestCase):
             filepath=DEFAULT_CONFIG_FILEPATH,
             overwrite=overwrite,
         )
-        assert_certificate_generated(
-            dirpath=os.path.dirname(DEFAULT_CONFIG_FILEPATH),
-            overwrite=overwrite,
-            create_certificate_pair_mock=create_certificate_pair_mock,
-        )
         self.assertEqual(result.exit_code, 0)
         self.assertIn("👍", result.output)
 
-    @patch("chia_tea.cli.config.create_certificate_pair")
     @patch("chia_tea.cli.config.create_default_config")
-    def test_config_init_creates_custom_config_correctly(
-        self, create_default_config_mock, create_certificate_pair_mock
-    ):
+    def test_config_init_creates_custom_config_correctly(self, create_default_config_mock):
         overwrite = False
         config_filepath = "./blabla/config.yml"
 
@@ -58,19 +35,11 @@ class TestConfigCmd(unittest.TestCase):
             filepath=config_filepath,
             overwrite=overwrite,
         )
-        assert_certificate_generated(
-            dirpath=os.path.dirname(config_filepath),
-            overwrite=overwrite,
-            create_certificate_pair_mock=create_certificate_pair_mock,
-        )
         self.assertEqual(result.exit_code, 0)
         self.assertIn("👍", result.output)
 
-    @patch("chia_tea.cli.config.create_certificate_pair")
     @patch("chia_tea.cli.config.create_default_config")
-    def test_config_init_handles_an_existing_file_correctly(
-        self, create_default_config_mock, create_certificate_pair_mock
-    ):
+    def test_config_init_handles_an_existing_file_correctly(self, create_default_config_mock):
 
         overwrite = False
         err_msg = "Config file already exists ..."
@@ -84,16 +53,12 @@ class TestConfigCmd(unittest.TestCase):
             filepath=DEFAULT_CONFIG_FILEPATH,
             overwrite=overwrite,
         )
-        create_certificate_pair_mock.assert_not_called()
         self.assertEqual(result.exit_code, 1)
         self.assertIn("⛈️", result.output)
         self.assertIn(err_msg, result.output)
 
-    @patch("chia_tea.cli.config.create_certificate_pair")
     @patch("chia_tea.cli.config.create_default_config")
-    def test_config_init_would_overwrite_if_asked(
-        self, create_default_config_mock, create_certificate_pair_mock
-    ):
+    def test_config_init_would_overwrite_if_asked(self, create_default_config_mock):
         overwrite = True
         result = runner.invoke(
             app,
@@ -102,11 +67,6 @@ class TestConfigCmd(unittest.TestCase):
         create_default_config_mock.assert_called_once_with(
             filepath=DEFAULT_CONFIG_FILEPATH,
             overwrite=overwrite,
-        )
-        assert_certificate_generated(
-            os.path.dirname(DEFAULT_CONFIG_FILEPATH),
-            overwrite=overwrite,
-            create_certificate_pair_mock=create_certificate_pair_mock,
         )
         self.assertEqual(result.exit_code, 0)
         self.assertIn("👍", result.output)
